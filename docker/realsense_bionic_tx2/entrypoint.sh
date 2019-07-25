d1e68420bfdd5e4a1fa815a252ae7198cba48fa3
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-07-25 17:03:49 -0400

    Add text editor and fix entrypoint

--- docker/realsense_bionic_tx2/entrypoint.sh
+++ docker/realsense_bionic_tx2/entrypoint.sh
@@ -2,7 +2,7 @@
 
 source "/root/catkin_ws/devel/setup.bash"
 
-export ROS_IP=192.168.12.10$1
-export ROS_MASTER_URI=http://192.168.12.100:11311
+export ROS_IP=$(echo $(hostname -I) | cut -d' ' -f 2)
+export ROS_MASTER_URI=http://192.168.12.$1:11311
 
-roslaunch realsense2_camera multi_robot_slam_realsense_camera.launch robot_id:=$1
+roslaunch realsense2_camera multi_robot_slam_realsense_camera.launch robot_id:=$2
