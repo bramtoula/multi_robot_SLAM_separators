ac878dbbe945d157c7a1feee1a9b85ea4deed971
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-08-05 11:07:39 -0400

    Save all mavros topics when logging

--- tx2_ressources/run_batman.sh
+++ tx2_ressources/run_batman.sh
@@ -18,22 +18,24 @@
 
 run_batman()
 {
-    sleep 1
+    sleep 10
     # sudo ip addr flush dev bat0
     # sudo ip addr flush dev wlan0
-    sudo iw dev ra0 set type ibss
-    sudo iw dev ra0 ibss join NVIDIA 5220
-    # sudo batctl if add ra0
+    iw dev ra0 set type ibss
+    iw dev ra0 ibss join NVIDIA 5220
+    batctl if add ra0
+    ifconfig bat0 192.168.10.150 netmask 255.255.255.0 up
+    # route add default gw 192.168.10.1
     # sudo ip link set up dev bat0
     # sudo ip addr flush dev bat0
     # sudo ifdown bat0 && sudo ifup --ignore-errors bat0
     # sudo echo -e "# interfaces(5) file used by ifup(8) and ifdown(8)\n# Include files from /etc/network/interfaces.d:\nsource-directory /etc/network/interfaces.d\n\n# The loopback network interface\nauto lo\niface lo inet loopback\n\nauto wlan0\niface wlan0 inet static\naddress 192.168.12.150\nnetmask 255.255.255.0\ngateway 192.168.12.1\nwpa-conf /etc/wpa_supplicant/wpa_supplicant.conf\n\nauto bat0 \niface bat0 inet static \naddress 192.168.123.3 \nnetmask 255.255.255.0 \ngateway 192.168.123.1 \npre-up /usr/sbin/batctl if add ra0" > /etc/network/interfaces
-    sudo echo -e "\nallow-hotplug bat0 \niface bat0 inet static \naddress 192.168.123.4 \nnetmask 255.255.255.0 \ngateway 192.168.123.1 \npre-up /usr/sbin/batctl if add ra0" >> /etc/network/interfaces
+    # sudo echo -e "\nallow-hotplug bat0 \niface bat0 inet static \naddress 192.168.123.4 \nnetmask 255.255.255.0 \ngateway 192.168.123.1 \npre-up /usr/sbin/batctl if add ra0" >> /etc/network/interfaces
 
-    # sudo echo -e "\n\nauto bat0 \niface bat0 inet static \naddress 192.168.123.3 \nnetmask 255.255.255.0 \ngateway 192.168.123.1 \npre-up /usr/sbin/batctl if add ra0" >> /etc/network/interfaces
-    sudo systemctl restart networking.service
+    # # sudo echo -e "\n\nauto bat0 \niface bat0 inet static \naddress 192.168.123.3 \nnetmask 255.255.255.0 \ngateway 192.168.123.1 \npre-up /usr/sbin/batctl if add ra0" >> /etc/network/interfaces
+    # sudo systemctl restart networking.service
 
-    sudo head -n -7 /etc/network/interfaces > /home/nvidia/temp.txt ; sudo mv /home/nvidia/temp.txt /etc/network/interfaces
+    # sudo head -n -7 /etc/network/interfaces > /home/nvidia/temp.txt ; sudo mv /home/nvidia/temp.txt /etc/network/interfaces
 }
 
 run_batman &
--- tx2_ressources/setup_tx_mistlab.md
+++ tx2_ressources/setup_tx_mistlab.md
@@ -218,7 +218,7 @@ sudo batctl o
 ## Install libraries needed for our code
 On the TX2 itself, we need libraries used in our Docker containers.
 ```
-sudo apt-get install rhash curl libuv1 libsqlite3-dev libsuitesparse-dev libfreenect-dev libdc1394-22-dev libglvnd-dev libopencv-contrib3.2 libmetis-dev libboost-all-dev libpcl-dev liblz4-dev libogre-1.9-dev liburdfdom-dev liblog4cxx-dev libtinyxml2-dev libassimp4 libyaml-cpp0.5v5
+sudo apt-get install rhash curl libuv1 libsqlite3-dev libsuitesparse-dev libfreenect-dev libdc1394-22-dev libglvnd-dev libopencv-contrib3.2 libmetis-dev libboost-all-dev libpcl-dev liblz4-dev libogre-1.9-dev liburdfdom-dev liblog4cxx-dev libtinyxml2-dev libassimp4 libyaml-cpp0.5v5 geographiclib-tools
 ```
 
 
