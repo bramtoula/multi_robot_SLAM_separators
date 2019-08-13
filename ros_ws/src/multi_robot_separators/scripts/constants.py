3b31731edbde9602a6811d90f3f8feaade955e84
Pierre-Yves Lajoie <lajoie.py@gmail.com>
2019-08-13 09:23:38 -0400

    Update parameters

--- ros_ws/src/multi_robot_separators/scripts/constants.py
+++ ros_ws/src/multi_robot_separators/scripts/constants.py
@@ -1,6 +1,6 @@
 BATCH_SIZE = 3 # Must be adapted to speed to run netvlad
 MAX_MATCHES = 20
-MATCH_DISTANCE = 0.135
+MATCH_DISTANCE = 0.13
 NETVLAD_DIMS = 128
 NB_KF_SKIPPED = 0 # Will use one in every (NB_KF_SKIPPED+1) keyframes
 MAX_QUEUE_SIZE = 5
