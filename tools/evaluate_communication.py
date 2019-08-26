6676e152b75f4a66e19b5b39090c01f4a813e6fe
Benjamin Ramtoula <benjaminramtoula@gmail.com>
2019-08-26 04:38:21 -0400

    Compute data exchange

--- tools/evaluate_communication.py
+++ tools/evaluate_communication.py
@@ -1,6 +1,11 @@
+import numpy as np
 
 path = '/Users/benjaminramtoula/Documents/Cours/POLYMTL/MISTLAB/SLAM/decentralized_slam_project/logs/'
 dir = '2019-08-22_11-43-20_0.13_20/'
+
+timestamp_combined = []
+nb_kf_used = []
+data_exchanged = []
 
 with open(path+dir+'find_matches_query.txt', 'r') as infile:
     lines = infile.readlines()
@@ -43,9 +48,69 @@ with open(path+dir+'find_matches_answer.txt', 'r') as infile:
 
 with open(path+dir+'receive_separators_query.txt', 'r') as infile:
     lines = infile.readlines()
-    r_s_a_timestamps = filter(lambda x: x.startswith('15'), lines)
-    r_s_a_timestamps = [float(t[:-2]) for t in f_m_a_timestamps]
-    r_s_a_number_of_kf_ids_from = filter(
+    r_s_q_timestamps = filter(lambda x: x.startswith('15'), lines)
+    r_s_q_timestamps = [float(t[:-2]) for t in r_s_q_timestamps]
+    r_s_q_number_of_kf_ids_from = filter(
         lambda x: x.startswith('number_of_kf_ids_from'), lines)
-    r_s_a_number_of_kf_ids_from = [
-        int(v.split()[1]) for v in f_m_a_number_of_kf_ids_from]
+    r_s_q_number_of_kf_ids_from = [
+        int(v.split()[1]) for v in r_s_q_number_of_kf_ids_from]
+
+
+while (not f_m_q_timestamps[0] == np.inf) or (not r_s_q_timestamps[0] == np.inf) or (not f_m_a_timestamps[0] == np.inf):
+    if (f_m_q_timestamps[0] < r_s_q_timestamps[0]) and (f_m_q_timestamps[0] < f_m_a_timestamps[0]):
+        if not f_m_q_number_of_values_in_descriptors[0] or f_m_q_number_of_values_in_descriptors[0] == 0:
+            f_m_q_timestamps.pop(0)
+            f_m_q_number_of_values_in_descriptors.pop(0)
+            if not f_m_q_timestamps:
+                f_m_q_timestamps.append(np.inf)
+            continue
+        timestamp_combined.append(f_m_q_timestamps[0])
+        nb_kf_used.append(int(f_m_q_number_of_values_in_descriptors[0]/128))
+        data_exchanged.append(f_m_q_number_of_values_in_descriptors[0]*8)
+        f_m_q_timestamps.pop(0)
+        f_m_q_number_of_values_in_descriptors.pop(0)
+        if not f_m_q_timestamps:
+            f_m_q_timestamps.append(np.inf)
+
+    elif (r_s_q_timestamps[0] < f_m_a_timestamps[0]):
+        if not r_s_q_number_of_kf_ids_from[0]:
+            r_s_q_timestamps.pop(0)
+            r_s_q_number_of_kf_ids_from.pop(0)
+            if not r_s_q_timestamps:
+                r_s_q_timestamps.append(np.inf)
+            continue
+        timestamp_combined.append(r_s_q_timestamps[0])
+        nb_kf_used.append(nb_kf_used[-1])
+        data_exchanged.append(2+(8+344*3)*r_s_q_number_of_kf_ids_from[0])
+        r_s_q_timestamps.pop(0)
+        r_s_q_number_of_kf_ids_from.pop(0)
+        if not r_s_q_timestamps:
+            r_s_q_timestamps.append(np.inf)
+
+    elif f_m_a_timestamps[0] < np.inf:
+        if f_m_a_number_of_kf_ids_computing_robot[0] == 0:
+            f_m_a_timestamps.pop(0)
+            f_m_a_sizes_of_kpts3D.pop(0)
+            f_m_a_sizes_of_descriptors.pop(0)
+            if not f_m_a_timestamps:
+                f_m_a_timestamps.append(np.inf)
+            continue
+        timestamp_combined.append(f_m_a_timestamps[0])
+        nb_kf_used.append(nb_kf_used[-1])
+
+        # print(f_m_a_sizes_of_kpts3D[0])
+        data_exchanged.append(
+            f_m_a_number_of_kf_ids_computing_robot[0]*(344+44*np.mean(f_m_a_sizes_of_kpts3D[0])+np.mean(f_m_a_sizes_of_descriptors[0])))
+
+        f_m_a_timestamps.pop(0)
+        f_m_a_sizes_of_kpts3D.pop(0)
+        f_m_a_sizes_of_descriptors.pop(0)
+        if not f_m_a_timestamps:
+            f_m_a_timestamps.append(np.inf)
+
+    else:
+        break
+
+
+print(data_exchanged)
+print(np.cumsum(data_exchanged))
